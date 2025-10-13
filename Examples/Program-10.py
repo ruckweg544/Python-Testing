097fe828af7e36eb8abba047226164185adbcf73
THX-1138 <139034465+ruckweg544@users.noreply.github.com>
2025-10-13 13:49:17 +0000

    Program 10 added

---
+++ Examples/Program-10.py
@@ -0,0 +1,11 @@
+# Program 10
+# Description: Swapping two variabls without temp variable
+# Date: 2025-10-13
+
+a = 5
+b = 10
+
+a, b = b, a
+print("After swapping")
+print("a = ", a)
+print("b = ", b)
