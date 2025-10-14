fa711e441a54891666d0496fba5acb4c6e6bf7d5
THX-1138 <139034465+ruckweg544@users.noreply.github.com>
2025-10-14 14:00:46 +0000

    Program 24 added

--- Examples/Program-24.py
+++ Examples/Program-24.py
@@ -1,3 +1,20 @@
 # Program 24
 # Description: Manually convert a decimal number to binary, octal and hexadecimal
 # Date: 2025-10-14
+
+''' ChatGPT Support for 'conversion_table' function'''
+def conversion_table(num):
+    # Prepare table content
+    table = [
+        ["Binary", bin(num)],         # R1
+        ["Octal", oct(num)],          # R2
+        ["Hexadecimal", hex(num)]     # R3
+    ]
+
+    # Compute max column widths for neat formatting
+    col_widths = [max(len(row[c]) for row in table) for c in range(2)]
+    for row in table:
+        print("| " + " | ".join(f"{row[c]:<{col_widths[c]}}" for c in range(2)) + " |")
+
+dec_num = int(input("Enter a decimal number: "))
+conversion_table(dec_num)
--- README.md
+++ README.md
@@ -5,3 +5,9 @@ The focus of this repository is to exercise basics ot Python. +1
 
 ## Example Program Descriptions
 - [x] Program 8: Calendar of the month
+- [x] Program 9: Quadratic Equation
+- [x] Program 10: Swapping two variabls without temp variable
+- [x] Program 14: Prime Number Determination
+- [x] Program 22: Least Common Multiple of two input number
+- [x] Program 23: Highest Common Factor of two input number
+- [x] Program 24: Decimal number to binary, octal and hexadecimal
